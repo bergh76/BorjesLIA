6dba2f182f9e077da82bf173e3a80281117271f2
Andreas Bergh <andreas__bergh@hotmail.com>
2016-05-02 22:00:10 +0200

    Added DtmModel

---
+++ BorjesLIA/BorjesLIA/Models/Diesel/DtmModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BorjesLIA.Models.Diesel
+{
+    public class DtmModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Display(Name = "Nytt DTMpris")]
+        public decimal dtmPrice { get; set; }
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Nytt Datum")]
+        public DateTime dateNewDieselPrice { get; set; }
+    }
+}

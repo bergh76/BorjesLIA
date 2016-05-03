cced0603e735cec9b210cdc676a64b0874b1ff72
Andreas <andreas__Bergh@hotmail.com>
2016-05-03 16:35:52 +0200

    Fixed some stuff

--- BorjesLIA/BorjesLIA/Models/StartModel.cs
+++ BorjesLIA/BorjesLIA/Models/StartModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BorjesLIA.Models.Diesel;
+using BorjesLIA.Models.Euro;
+using BorjesLIA.Models.Video;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +10,12 @@ namespace BorjesLIA.Models
 {
     public class StartModel
     {
-
-
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DieselPriceModel DieselSlide { get; set; }
+        public DtmModel DmtSlide { get; set; }
+        public GetEuroExchangeModel EuroSlide { get; set; }
+        public UrlModel UrlSlide { get; set; }
+        public VideoModel VideoSlide { get; set; }
     }
 }

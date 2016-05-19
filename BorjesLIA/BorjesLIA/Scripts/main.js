78ea94d638235e9eaeab8737ded0e2cd4aa6dcc6
Andreas <andreas__Bergh@hotmail.com>
2016-05-19 16:04:31 +0200

    Trying to fix iframe styling with responsive height and width

--- BorjesLIA/BorjesLIA/Scripts/main.js
+++ BorjesLIA/BorjesLIA/Scripts/main.js
@@ -1,35 +1,71 @@
-﻿// ToDo: Function that maximizes the containter with the bxSlider and explicit set width and hight to fixed values.
+﻿// BXSLIDER JQUERY //
+//$(document).ready(function () {
+//    $(".bxslider").bxSlider({
+//        pager: false,
+//        adaptiveHeight: true,
+//        mode: "fade",
+//        loading: true,
+//        controls: false,
+//        //pause: 5000, //15000,
+//        //video: true,
+//        autoControls: false,
+//    });
+    // BXSLIDER JQUERY //
 
-    function toggleFullScreen() {
-        if (!document.fullscreenElement &&    // alternative standard method
-            !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement) {  // current working methods
-            if (document.documentElement.requestFullscreen) {
-                document.documentElement.requestFullscreen();
-            } else if (document.documentElement.msRequestFullscreen) {
-                document.documentElement.msRequestFullscreen();
-            } else if (document.documentElement.mozRequestFullScreen) {
-                document.documentElement.mozRequestFullScreen();
-            } else if (document.documentElement.webkitRequestFullscreen) {
-                document.documentElement.webkitRequestFullscreen(Element.ALLOW_KEYBOARD_INPUT);
-            }
-            $(document).ready(function () {
-                $('nav').fadeOut('slow');
-            });
-           
+    //Trigger Fullscreen
+    //function launchIntoFullscreen(element) {
+    //    if ((document.fullScreenElement !== undefined && document.fullScreenElement === null) || (document.msFullscreenElement !== undefined && document.msFullscreenElement === null) || (document.mozFullScreen !== undefined && !document.mozFullScreen) || (document.webkitIsFullScreen !== undefined && !document.webkitIsFullScreen)) {
+    //        if (element.requestFullscreen) {
+    //            element.requestFullscreen();
+    //        } else if (element.mozRequestFullScreen) {
+    //            element.mozRequestFullScreen();
+    //        } else if (element.webkitRequestFullscreen) {
+    //            element.webkitRequestFullscreen();
+    //        } else if (element.msRequestFullscreen) {
+    //            element.msRequestFullscreen();
+    //        }
+    //    } else {
+    //        //Whack fullscreen
+    //        if (document.exitFullscreen) {
+    //            document.exitFullscreen();
+    //        } else if (document.mozCancelFullScreen) {
+    //            document.mozCancelFullScreen();
+    //        } else if (document.webkitExitFullscreen) {
+    //            document.webkitExitFullscreen();
+    //        }
+    //    }
+    //}
 
-        } else {
-           
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            } else if (document.msExitFullscreen) {
-                document.msExitFullscreen();
-            } else if (document.mozCancelFullScreen) {
-                document.mozCancelFullScreen();
-            } else if (document.webkitExitFullscreen) {
-                document.webkitExitFullscreen();
-            }
-           
-        }
+    //Hide navbar on Home/Index
+    //$('#navbar-start').hide();
+    //$("html").mousemove(function (event) {
+    //    $('#navbar-start').show();
+    //    myStopFunction();
+    //    myFunction();
+    //});
+    //function myFunction() {
+    //    myVar = setTimeout(function () {
+    //        $('#navbar-start').hide();
+    //    }, 1000);
+    //}
+    //function myStopFunction() {
+    //    if (typeof myVar != 'undefined') {
+    //        clearTimeout(myVar);
+    //    }
+    //}
 
-    }
+    ////video test
+    //$("#videoOne").on("pause", function (e) {
+    //    slider.startShow();
+    //});
+
+    //$("#videoOne").on("play", function (e) {
+    //    slider.stopShow();
+    //});
+    //});
+});
+
+
+// ToDo: Function that maximizes the containter with the bxSlider and explicit set width and hight to fixed values.
+
    

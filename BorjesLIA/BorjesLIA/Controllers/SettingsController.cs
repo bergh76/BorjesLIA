c5661c71136b7eb95df43d2911e75970bdd6a414
Andreas Bergh <andreas__bergh@hotmail.com>
2016-05-23 22:22:55 +0200

    All inputdatapages has now functional settingspage. Still need function for update chart after values are saved to database.

--- BorjesLIA/BorjesLIA/Controllers/SettingsController.cs
+++ BorjesLIA/BorjesLIA/Controllers/SettingsController.cs
@@ -21,6 +21,8 @@ namespace BorjesLIA.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
         string confEuroSettings = "Eurokurs";
         string confDtmSettings = "Drivmedelstillägg";
+        string confDwSettings = "Dieselpris Vecka";
+        string confDqSettings = "Dieselpris Kvartal";
         // GET: Settings
         //public ActionResult Index(EuroViewModel euroV)
         //{
@@ -94,8 +96,9 @@ namespace BorjesLIA.Controllers
         }
 
 
-
-
+        //******************************************************************************************//
+        //*******************************| DRIVMEDELSTILLÄGSETTEING |*******************************//
+        //******************************************************************************************//
         /// <summary>
         /// Creats a new EuroViewModel object, populates needed lists with data and return a view with data
         /// </summary>
@@ -155,6 +158,141 @@ namespace BorjesLIA.Controllers
             }
             return View(confDtm);
         }
+
+        //******************************************************************************************//
+        //**********************************| DIESELWEEKSETTINGS |**********************************//
+        //******************************************************************************************//
+        /// <summary>
+        /// Creats a new EuroViewModel object, populates needed lists with data and return a view with data
+        /// </summary>
+        /// <param name="dwSettings"></param>
+        /// <returns></returns>
+        public ActionResult Index_DieselWeekSettings(DieselWeekViewModel dwSettings) // try select byt using FormeCollection and insert Year as grouppolicy
+        {
+            dwSettings = new DieselWeekViewModel
+            {
+                AddWeekDiesel = new DieselWeekModel(),
+                newWeekDieselList = db.DieselPriceWeek.OrderByDescending(x => x.Week).ToList(),
+                //populates list used for determain charttype from Entity Settings
+                settings = db.Settings.Where(x => x.Name == confDwSettings)
+            };
+            return View(dwSettings);
+        }
+
+        /// <summary>
+        /// Populates the chart with data
+        /// </summary>
+        /// <param name="dtmData"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        //Populates a list with data from database tabel EuroExchangeModel
+        public async Task<JsonResult> GetDWData(DieselWeekViewModel dwData)
+        {
+            var data = await dwData.GetWeekData();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Creats a Settings object and collects data from inputform via FormCollection and saves new values to Settings Entity
+        /// </summary>
+        /// <param name="confDw"></param>
+        /// <param name="formDtm"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult SaveDWSettings(Settings confDw, FormCollection formDtm)
+        {
+            // Saves new settings to Entity Settings
+            if (Request.IsAjaxRequest() && ModelState.IsValid)
+            {
+                // populate values from Html-form
+                string name = formDtm[1].ToString();
+                confDw.ID = db.Settings.Where(x => x.Name == confDwSettings).Select(x => x.ID).FirstOrDefault();
+                confDw.Year = Convert.ToInt32(formDtm[2]);
+                confDw.Name = this.confDwSettings;
+                if (!string.IsNullOrEmpty(confDw.Name) && confDw.Year != 0)
+                {
+                    // saves data to Settings db Entity
+                    db.Entry(confDw).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return PartialView("Index_DieselWeekSettings", confDw);
+                }
+
+            }
+            return View(confDw);
+        }
+
+
+        //******************************************************************************************//
+        //********************************| DIESELQUARTERSETTINGS |*********************************//
+        //******************************************************************************************//
+
+        //******************************************************************************************//
+        //**********************************| DIESELWEEKSETTINGS |**********************************//
+        //******************************************************************************************//
+        /// <summary>
+        /// Creats a new EuroViewModel object, populates needed lists with data and return a view with data
+        /// </summary>
+        /// <param name="dqSettings"></param>
+        /// <returns></returns>
+        public ActionResult Index_DieselQuarterSettings(DieselQuarterViewModel dqSettings) // try select byt using FormeCollection and insert Year as grouppolicy
+        {
+            dqSettings = new DieselQuarterViewModel
+            {
+                AddQuarterDiesel = new DieselQuarterPriceModel(),
+                newQuarterDieselList = db.DieselPriceQuarter.OrderByDescending(x => x.Quarter).ToList(),
+                //populates list used for determain charttype from Entity Settings
+                settings = db.Settings.Where(x => x.Name == confDqSettings)
+            };
+            return View(dqSettings);
+        }
+
+        /// <summary>
+        /// Populates the chart with data
+        /// </summary>
+        /// <param name="dtmData"></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        //Populates a list with data from database tabel EuroExchangeModel
+        public async Task<JsonResult> GetDQData(DieselQuarterViewModel dqData)
+        {
+            var data = await dqData.GetQuaerterData();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+
+        /// <summary>
+        /// Creats a Settings object and collects data from inputform via FormCollection and saves new values to Settings Entity
+        /// </summary>
+        /// <param name="confDq"></param>
+        /// <param name="formDq"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult SaveDQSettings(Settings confDq, FormCollection formDq)
+        {
+            // Saves new settings to Entity Settings
+            if (Request.IsAjaxRequest() && ModelState.IsValid)
+            {
+                // populate values from Html-form
+                string name = formDq[1].ToString();
+                confDq.ID = db.Settings.Where(x => x.Name == confDqSettings).Select(x => x.ID).FirstOrDefault();
+                confDq.Year = Convert.ToInt32(formDq[2]);
+                confDq.Name = this.confDqSettings;
+                if (!string.IsNullOrEmpty(confDq.Name) && confDq.Year != 0)
+                {
+                    // saves data to Settings db Entity
+                    db.Entry(confDq).State = EntityState.Modified;
+                    db.SaveChanges();
+
+                    return PartialView("Index_DieselQuarterSettings", confDq);
+                }
+
+            }
+            return View(confDq);
+        }
+
+
+
+
 
 
         protected override void Dispose(bool disposing)

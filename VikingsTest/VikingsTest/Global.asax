6e41c461ddccc464dd211b12a30237b390614819
Andreas Bergh <andreas__bergh@hotmail.com>
2016-07-04 18:25:40 +0200

    Buttons

---
+++ VikingsTest/VikingsTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="VikingsTest.MvcApplication" Language="C#" %>
---
+++ VikingsTest/VikingsTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VikingsTest.Startup))]
+namespace VikingsTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
